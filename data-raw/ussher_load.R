0af2702f8c1189a1314d97b8541a90be9ef20465
DataSciNet <90725067+DataSciNet@users.noreply.github.com>
2022-07-18 14:17:45 -0500

    add dplyr

--- data-raw/ussher_load.R
+++ data-raw/ussher_load.R
@@ -1,10 +1,29 @@
 library(tidyverse)
 library(stringr)
 library(tidytext)
+library(tidyr)
+library(dplyr)
 annals.data <-
   read.csv("~/RStudio Files/packageussh/ussher/ussher/data-raw/UssherTextPreclean4.csv", stringsAsFactors = FALSE, skip = 11,sep=";")
 annals.data <- as_tibble(annals.data)
 annals.data.na1 <- annals.data %>% dplyr::na_if("AgeBlank") %>% dplyr::na_if("DateBlank")
-annals.data <- annals.data.na1 %>% fill(Epoch) %>% fill(Dating)
+annals.data.fill1 <- annals.data.na1 %>% fill(Epoch) %>% fill(Dating)
+annals.index <- annals.data.fill1 %>%
+  mutate(Index = as.numeric(str_extract(Event, "[0-9]+\\."))) %>%
+  mutate(TextSrc1 = as.character(str_extract_all(Event, "[A-Z][a-z]+ [0-9]+:[0-9]++-[0-9]+"))) %>%
+  mutate(BibBk1 = as.character(str_extract(TextSrc1, "[A-Z][a-z]+")))
+ussh.ind <- annals.index %>%
+  mutate(EventTxt=(str_replace_all(Event, "[A-Z][a-z]+ [0-9]+:[0-9]++-[0-9]"," "))) %>%
+  mutate(EventTxt=(str_replace_all(EventTxt, "[[:punct:]]"," "))) %>%
+  mutate(EventTxt=(str_replace_all(EventTxt, "[0-9]+"," "))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The Seventh Age of the World","7th Age"))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The Sixth Age of the World","6th Age"))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The Fifth Age of the World","5th Age"))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The Fourth Age of the World","4th Age"))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The Third Age of the World","3rd Age"))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The Second Age of the World","2nd Age"))) %>%
+  mutate(Epoch=(str_replace_all(Epoch,"The First Age of the World","1st Age")))
+ussh.ind[ussh.ind == "character(0)" ] <- NA
+annals.data <- ussh.index
 usethis::use_data(annals.data, overwrite = TRUE)
 
