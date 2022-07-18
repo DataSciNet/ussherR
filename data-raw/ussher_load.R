223e600cfae486979ac072a2a52303e46cc57a90
DataSciNet <90725067+DataSciNet@users.noreply.github.com>
2022-07-18 13:57:09 -0500

    revert to annals.data

--- data-raw/ussher_load.R
+++ data-raw/ussher_load.R
@@ -5,5 +5,6 @@ annals.data <-
   read.csv("~/RStudio Files/packageussh/ussher/ussher/data-raw/UssherTextPreclean4.csv", stringsAsFactors = FALSE, skip = 11,sep=";")
 annals.data <- as_tibble(annals.data)
 annals.data.na1 <- annals.data %>% dplyr::na_if("AgeBlank") %>% dplyr::na_if("DateBlank")
-annals.data.fill1 <- annals.data.na1 %>% fill(Epoch) %>% fill(Dating)
-usethis::use_data(annals.data.fill1, overwrite = TRUE)
+annals.data <- annals.data.na1 %>% fill(Epoch) %>% fill(Dating)
+usethis::use_data(annals.data, overwrite = TRUE)
+
