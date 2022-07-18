0d05fc96670416f9533dbee6ce88f0f676ced6cf
DataSciNet <90725067+DataSciNet@users.noreply.github.com>
2022-07-18 09:01:00 -0500

    fill variables for dataset

--- data-raw/ussher_load.R
+++ data-raw/ussher_load.R
@@ -1,5 +1,9 @@
 library(tidyverse)
+library(stringr)
+library(tidytext)
 annals.data <-
   read.csv("~/RStudio Files/packageussh/ussher/ussher/data-raw/UssherTextPreclean4.csv", stringsAsFactors = FALSE, skip = 11,sep=";")
 annals.data <- as_tibble(annals.data)
+annals.data.na1 <- annals.data %>% dplyr::na_if("AgeBlank") %>% dplyr::na_if("DateBlank")
+annals.data.fill1 <- annals.data.na1 %>% fill(Epoch) %>% fill(Dating)
 usethis::use_data(annals.data, overwrite = TRUE)
